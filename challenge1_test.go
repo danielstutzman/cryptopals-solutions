75a75f906eae8f7bd80d02d656fdd9a04e992064
Dan Stutzman <dan@danstutzman.com>
2014-11-11 15:12:40 -0700

    Refactor Challenge 1 to split HexToBase64 into HexToBytes and BytesToBase64.

--- challenge1_test.go
+++ challenge1_test.go
@@ -5,8 +5,30 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
+func TestHexToBytes(t *testing.T) {
+  bytes1, err := HexToBytes("40f5")
+  assert.Equal(t, nil, err)
+  assert.Equal(t, []byte("\x40\xf5"), bytes1)
+}
+
+func TestHexToBytesInvalidChar(t *testing.T) {
+  _, err := HexToBytes("zz")
+  assert.NotEqual(t, nil, err)
+}
+
+func TestHexToBytesInvalidLen(t *testing.T) {
+  _, err := HexToBytes("123")
+  assert.NotEqual(t, nil, err)
+}
+
+func TestBytesToBase64(t *testing.T) {
+  assert.Equal(t, "c3VyZS4=", BytesToBase64([]byte("sure.")))
+}
+
 func TestHexToBase64(t *testing.T) {
+  bytes1, err := HexToBytes("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+  assert.Equal(t, nil, err)
   assert.Equal(t,
     "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
-    HexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
+    BytesToBase64(bytes1))
 }
--- library.go
+++ library.go
@@ -3,24 +3,41 @@ package cryptopals
 import (
   "bytes"
   "encoding/base64"
-//  "fmt"
+  "fmt"
 )
 
-func HexToBase64(hex string) string {
-  var base64Out bytes.Buffer
-  base64Encoder := base64.NewEncoder(base64.StdEncoding, &base64Out)
+func HexToBytes(hex string) ([]byte, error) {
+  var buffer bytes.Buffer
 
-  hexDigitToValue := map[rune]int{
+  if len(hex) % 2 != 0 {
+    return nil, fmt.Errorf("Input doesn't have even number of runes: %s", hex)
+  }
+
+  hexDigitToValue := map[rune]uint8{
     '0': 0, '1': 1, '2':  2, '3':  3, '4':  4, '5':  5, '6':  6, '7':  7,
     '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15,
+                    'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15,
   }
   for i := 0; i < len(hex); i += 2 {
     c1 := rune(hex[i])
     c2 := rune(hex[i + 1])
-    combined := hexDigitToValue[c1] * 16 + hexDigitToValue[c2]
-    base64Encoder.Write([]byte(string(combined)))
-  }
-  base64Encoder.Close()
 
-  return base64Out.String()
+    v1, found1 := hexDigitToValue[c1]
+    if !found1 {
+      return nil, fmt.Errorf("Bad hex digit %c in %s", c1, hex)
+    }
+    v2, found2 := hexDigitToValue[c2]
+    if !found2 {
+      //panic(fmt.Sprintf("Bad hex digit %c in %s", c1, hex))
+    }
+
+    var combined uint8 = v1 * 16 + v2
+    buffer.Write([]byte{combined})
+  }
+
+  return buffer.Bytes(), nil
+}
+
+func BytesToBase64(bytes []byte) string {
+  return base64.StdEncoding.EncodeToString(bytes)
 }
